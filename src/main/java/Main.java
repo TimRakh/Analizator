09a1c7eec04cb84188da82c57cbf23e0e8eba991
Tim Rakh <nbveh0914@gmail.com>
2023-04-24 16:53:52 +0300

    Commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,60 @@
+import java.util.Random;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class Main {
+    public static final BlockingQueue<String> queue1 = new ArrayBlockingQueue<>(100);
+    public static final BlockingQueue<String> queue2 = new ArrayBlockingQueue<>(100);
+    public static final BlockingQueue<String> queue3 = new ArrayBlockingQueue<>(100);
+    public static final int textLength = 100_000;
+    public static final int wordsAmount = 10_000;
+
+    public static void main(String[] args) {
+        String[] words = new String[wordsAmount];
+        Thread queue = new Thread(()->{
+        for (int i = 0; i < words.length; i++) {
+            words[i] = generateText("abc",textLength);
+            try {
+                queue1.put(words[i]);
+                queue2.put(words[i]);
+                queue3.put(words[i]);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }});
+        queue.start();
+        Thread queueA = new Thread(()->calc('a',queue1));
+        queueA.start();
+        Thread queueB = new Thread(()->calc('b',queue2));
+        queueB.start();
+        Thread queueC = new Thread(()->calc('c',queue3));
+        queueC.start();
+
+    }
+    public static String generateText(String letters, int length) {
+        Random random = new Random();
+        StringBuilder text = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            text.append(letters.charAt(random.nextInt(letters.length())));
+        }
+        return text.toString();
+    }
+    public static void calc(char bukva, BlockingQueue<String> textsQueue) {
+        int counter = 0;
+
+        for (int i = 0; i < wordsAmount; i++) {
+            try {
+                String word = textsQueue.take();
+                for (int j = 0; j < word.length(); j++) {
+                    if (word.charAt(j) == bukva) {
+                        counter++;
+                    }
+                }
+
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        System.out.println("Наибольшее количество букв " + bukva + " : " + counter);
+    }
+}
